76cf68a194571d1cdc8c7cb8e1237f985705b770
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2024-07-12 16:51:42 -0300

    tested and ok and savepoint

--- db/upgrade.php
+++ db/upgrade.php
@@ -7,6 +7,7 @@ function xmldb_tool_sentry_upgrade($oldversion): bool {
 
     if ($oldversion < 2024071200) {
         $DB->delete_records("mdl_config_plugins",[ 'plugin'=>'tool_sentry','name'=>'dns']);
+        upgrade_plugin_savepoint(true, 2024071200, 'tool', 'sentry');
     }
 
     return true;
