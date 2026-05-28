0e5588f3454cd52d89f0a3c12780f84350a20758
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2026-05-28 11:31:48 -0300

    new release to moodle marketplace

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // Plugin version.
-$plugin->version = 2025102400;
+$plugin->version = 2026052000;
 
 // Required Moodle version.
 $plugin->requires = 2022112800;
